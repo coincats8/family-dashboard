bfe8dc474f5cf9ab655515040774614ae39f66f5
coincats8 <132332393+coincats8@users.noreply.github.com>
2026-08-25 11:41:26 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,87 +1,46 @@
-// ================================
-// Family Dashboard v0.2
-// ================================
+// =======================================
+// Family Dashboard
+// API Version
+// =======================================
 
-// 仮データ（後でスプレッドシートから取得）
-const dashboard = {
-  budget: 300000,
-  total: 152380,
-  categories: {
-    food: 48230,
-    eatingOut: 12830,
-    daily: 8520,
-    transport: 5980
-  }
-};
+const API =
+"https://script.google.com/macros/s/AKfycbxfmSB9YZCQ5aWsU5yyl3DB2dB6egoz5Y5noF0zGM2cc7ID3jl0DuMh0uNWlguM67s/exec?mode=dashboard";
 
-// 金額を「¥123,456」にする
-function yen(value) {
-  return "¥" + value.toLocaleString("ja-JP");
-}
+function yen(value){
 
-// カウントアップ
-function animateNumber(target, end) {
-  let current = 0;
-
-  const step = Math.max(1, Math.ceil(end / 80));
-
-  const timer = setInterval(() => {
-
-    current += step;
-
-    if (current >= end) {
-      current = end;
-      clearInterval(timer);
-    }
-
-    target.textContent = yen(current);
-
-  }, 16);
-}
-
-// 今日の日付
-function setMonth() {
-
-  const now = new Date();
-
-  const month = document.querySelector(".month");
-
-  month.textContent =
-    now.getFullYear() +
-    "年" +
-    (now.getMonth() + 1) +
-    "月";
+    return "¥"+Number(value).toLocaleString("ja-JP");
 
 }
 
-// 初期表示
-window.onload = () => {
+async function loadDashboard(){
 
-  setMonth();
+    const response = await fetch(API);
 
-  // 合計金額
-  const total = document.getElementById("totalMoney");
+    const data = await response.json();
 
-  animateNumber(total, dashboard.total);
+    console.log(data);
 
-  // プログレスバー
-  const percent = dashboard.total / dashboard.budget * 100;
+    // 合計
+    document.getElementById("totalMoney").textContent =
+        yen(data.expense);
 
-  document.querySelector(".progress-bar").style.width =
-    percent + "%";
+    // プログレスバー
+    const percent =
+        data.expense / data.budget * 100;
 
-  // 残り予算
-  document.querySelector(".budget-row").innerHTML = `
-    <span>予算 ${yen(dashboard.budget)}</span>
-    <span>残り ${yen(dashboard.budget-dashboard.total)}</span>
-  `;
+    document.querySelector(".progress-bar").style.width =
+        percent + "%";
 
-  // カテゴリ
-  const rows = document.querySelectorAll(".category strong");
+    // 予算
+    document.querySelector(".budget-row").innerHTML=`
+        <span>予算 ${yen(data.budget)}</span>
+        <span>残り ${yen(data.balance)}</span>
+    `;
 
-  rows[0].textContent = yen(dashboard.categories.food);
-  rows[1].textContent = yen(dashboard.categories.eatingOut);
-  rows[2].textContent = yen(dashboard.categories.daily);
-  rows[3].textContent = yen(dashboard.categories.transport);
+}
 
-};
+window.onload=()=>{
+
+    loadDashboard();
+
+}
